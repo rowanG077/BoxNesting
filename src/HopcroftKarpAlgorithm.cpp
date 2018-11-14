99493e2826bac4a4edf5dddd533d0c4c4900bd79
RowanG077 <goemansrowan@gmail.com>
2018-11-14 20:35:59 +0100

    Added code comments explaining Hopcroft-Karp algorithm

--- src/HopcroftKarpAlgorithm.cpp
+++ src/HopcroftKarpAlgorithm.cpp
@@ -9,18 +9,23 @@
 namespace BoxNesting
 {
 
-const int16_t HopcroftKarpAlgorithm::NILL = 0; // NOLINT
-const int16_t HopcroftKarpAlgorithm::INF = std::numeric_limits<int16_t>::max(); // NOLINT
+const int16_t HopcroftKarpAlgorithm::NILL = 0; // NOLINT, because of formatting
+const int16_t HopcroftKarpAlgorithm::INF = std::numeric_limits<int16_t>::max(); // NOLINT, because of formatting
 
 HopcroftKarpAlgorithm::HopcroftKarpAlgorithm(const std::vector<Box>& boxes)
 {
-	// In the boxes case left and right vertices are the same amount since
-	// they are the amount of boxes
+	// In the boxes case the left and right vertices are the same amount since
+	// they are the amount of boxes. Since it never exceeds 5000 we can easily
+	// cast here
 	this->leftVerticesCount = static_cast<int16_t>(boxes.size());
 	this->rightVerticesCount = this->leftVerticesCount;
 
 	this->graph.resize(this->leftVerticesCount + 1);
 
+	// This algorithm actually only requires the adjacency list
+	// of the left part of the graph so we only construct
+	// that. This is because in our boxes case the partitions
+	// contain the same amount of vertices.
 	for (int16_t i = 1; i <= this->leftVerticesCount; ++i) {
 		this->graph[i].reserve(this->rightVerticesCount + 1);
 		for (int16_t j = 1; j <= this->rightVerticesCount; ++j) {
@@ -39,7 +44,7 @@ int16_t HopcroftKarpAlgorithm::runAlgorithm() const
 	this->distances = std::vector<int16_t>(this->leftVerticesCount + 1, 0);
 
 	// We have the same amount of visible boxes as there
-	// are vertices in the left part of the bipartite graph
+	// are vertices in the left partition
 	// for each valid match (i.e. a box can nest inside another box)
 	// we remove a visible box
 	int16_t visibleBoxes = this->leftVerticesCount;
@@ -61,6 +66,10 @@ int16_t HopcroftKarpAlgorithm::runAlgorithm() const
 
 bool HopcroftKarpAlgorithm::dfs(int16_t vertex) const
 {
+	// Path ends if it visits the NILL vertex
+	// because we can't traverse anywhere from it
+	// It is a shorter path because we we are allready
+	// iterating through shorter paths.
 	if (vertex == NILL)
 	{
 		return true;
@@ -72,9 +81,11 @@ bool HopcroftKarpAlgorithm::dfs(int16_t vertex) const
 		if (this->distances[this->pairsRight[j]] == this->distances[vertex] + 1) 
 		{ 
 			// If dfs for pair of v also returns 
-			// true 
+			// true then it means the path ends at a
+			// NILL which means it is the shortest math
 			if (dfs(this->pairsRight[j])) 
 			{
+				// At the matching
 				this->pairsRight[j] = vertex;
 				this->pairsLeft[vertex] = j;
 				return true; 
@@ -82,8 +93,13 @@ bool HopcroftKarpAlgorithm::dfs(int16_t vertex) const
 		} 
 	} 
 
-	// If there is no augmenting path beginning with u. 
+	// We set distance to infinite here so we can ensure it will
+	// not be used in a future path. We allready know this is not
+	// the shortest path so it doesn't make sense to check this vertex
+	// again.
 	this->distances[vertex] = INF; 
+	// No paths where found to NILL so it isn't
+	// the shortest path
 	return false; 
 }
 
@@ -91,45 +107,49 @@ bool HopcroftKarpAlgorithm::bfs() const
 {
 	std::queue<int16_t> queue;
 
-	// First layer of vertices (set distance as 0) 
+	// Iterate through first layer of the graph from the left
+	// partition of the bipartite graph. If there is a vertex
+	// that doesn't have a pair we add it to the queue
+	// since we can use that vertex to traverse.
+	// If it allready has a pair we set the distance to
+	// to infinite because we can traverse it later from
+	// left part of the graph
 	for (int16_t i = 1; i <= this->leftVerticesCount; ++i) 
 	{ 
-		// If this is a free vertex, add it to queue 
 		if (this->pairsLeft[i] == NILL) 
 		{ 
 			this->distances[i] = 0; 
 			queue.push(i); 
 		} 
-		// Else set distance as infinite
-		// so that this vertex 
-		// is considered next time 
 		else
 		{
 			this->distances[i] = INF;
 		}
-	} 
+	}
 
-	// Initialize distance to NIL as infinite 
+	// If find a shortest augmented path we end at NILL and thus
+	// the distance would become the length of the path
+	// We use this information to eleminate path that are not the shortest
+	// since we can allready see that any path that is longer then this
+	// distance is not the shortest path.
 	this->distances[NILL] = INF;
 
-	// the queue can only contain vertices off the left part of the bipartite graph  
+	// Continue traversing through the layers
+	// While nodes in the partition are unmatched
+	// or have a previous match. If that is not the case
+	// the next vertex is NILL and we are getting there first
+	// time this run
 	while (!queue.empty()) 
 	{ 
-		// Dequeue a vertex 
 		auto i = queue.front(); 
 		queue.pop(); 
 
-		// If this node is not NIL and can provide a shorter path to NIL 
 		if (this->distances[i] < this->distances[NILL]) 
 		{ 
-			// Get all adjacent vertices of the dequeued vertex u
 			for (auto j : this->graph[i])
 			{
-				// If pair of v is not considered so far 
-				// (v, pairV[V]) is not yet explored edge. 
 				if (this->distances[this->pairsRight[j]] == INF) 
 				{ 
-					// Consider the pair and add it to queue 
 					this->distances[this->pairsRight[j]] = this->distances[i] + 1; 
 					queue.push(this->pairsRight[j]); 
 				} 
@@ -138,7 +158,8 @@ bool HopcroftKarpAlgorithm::bfs() const
 	} 
 
 	// If we could come back to NIL using alternating path of distinct 
-	// vertices then there is an augmenting path 
+	// vertices that is shorter then before then distance to null must
+	// be set
 	return this->distances[NILL] != INF; 
 }
 
--- src/HopcroftKarpAlgorithm.hpp
+++ src/HopcroftKarpAlgorithm.hpp
@@ -13,6 +13,8 @@ namespace BoxNesting
 {
 /**
  * @brief class that contains the boxnesting algorithm
+ *        Implemented with reference pseudocode from wikipedia:
+ *        https://en.wikipedia.org/wiki/Hopcroft%E2%80%93Karp_algorithm#Pseudocode
  */
 class HopcroftKarpAlgorithm
 {
@@ -23,7 +25,7 @@ public:
 	HopcroftKarpAlgorithm() = delete;
 
 	/**
-	 * @brief Construct a new Algorithm object
+	 * @brief Construct a new HopcroftKarpAlgorithm object
 	 *
 	 * @param boxes The boxes to generate the internal graph for
 	 */
@@ -31,24 +33,38 @@ public:
 
 	/**
 	 * @brief function that runs the boxNesting algorithm and returns the number
-	 * of visible boxes
+	 *        of visible boxes
 	 *
 	 * @param boxes the set of boxes to be nested
 	 * @return the number of visible boxes after running the nesting
-	 * algorithm
+	 *         algorithm
 	 */
 	[[nodiscard]] int16_t runAlgorithm() const;
 
 private:
-	bool bfs() const;
-	bool dfs(int16_t vertex) const;
+	/**
+	 * @brief We find all available shortest paths augmenting paths in the graph
+	 *        
+	 * @return True if any shortest augmenting path could be found. False if not 
+	 */
+	[[nodiscard]] bool bfs() const;
+
+	/**
+	 * @brief Use the results from bfs()
+	 * (i.e. the distances traveled from a free vertex in the part of the bipartite graph)
+	 * to find the shortest augmente path
+	 * 
+	 * @param vertex The vertex in the left partition to start dfs from
+	 * @return True if the shortest augmented path was found
+	 */
+	[[nodiscard]] bool dfs(int16_t vertex) const;
 
 private:
 	/**
 	 * @brief adjacencyList of left vertices generated from the boxes
-	 *		  First index on vector is a vertex on the left side of the bipartite graph
-	 *        and the vector on that index contains indices of vertex on the right side of
-	 *        of the graph where an edge exists between them.
+	 *        First index on vector is a vertex on the left side of the bipartite graph
+	 *        and the vector on that index contains indices of vertices on the right side of
+	 *        of that are connected to the vertex on the current index.
 	 */
 	std::vector<std::vector<int16_t>> graph;
 
@@ -61,16 +77,6 @@ private:
 	 * @brief The amount of vertices in the left side of the bipartite graph
 	 */
 	int16_t rightVerticesCount;
-
-	/**
-	 * @brief The index of the NILL node in the left part of the bipartite graph  
-	 */
-	static const int16_t NILL; // NOLINT
-
-	/**
-	 * @brief Used as marker for infinite distance
-	 */
-	static const int16_t INF; // NOLINT
 
 	/**
 	 * @brief Contains whether vertices on the left side of the bipartite graph are
@@ -89,9 +95,19 @@ private:
 	mutable std::vector<int16_t> pairsRight;
 
 	/**
-	 * @brief Stores distances of left vertices
+	 * @brief Stores distances from left free vertex
 	 */
 	mutable std::vector<int16_t> distances;
+
+	/**
+	 * @brief The index of the NILL node in the left part of the bipartite graph
+	 */
+	static const int16_t NILL; // NOLINT, because of formatting
+
+	/**
+	 * @brief Used as marker for infinite distance
+	 */
+	static const int16_t INF; // NOLINT, because of formatting
 };
 
 } // namespace BoxNesting
